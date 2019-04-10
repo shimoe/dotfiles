a785a7e20f0e64602773be606ac17353c7112658
shimoe <shimoewithranobe@gmail.com>
2019-04-10 13:59:25 +0900

    [edit] change ROS version

--- rosinstall.sh
+++ rosinstall.sh
@@ -1,7 +1,7 @@
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list' 
-wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add - 
-sudo apt-get update
-sudo apt-get install ros-indigo-desktop-full 
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt update
+sudo apt install ros-kinetic-desktop-full 
 sudo rosdep init
 rosdep update
 sudo apt-get install python-rosinstall
